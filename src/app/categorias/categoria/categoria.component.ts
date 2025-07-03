f18a89dae56a197a823d23c951b8553cfd07005d
Camila <camilaferna140494@gmail.com>
2025-07-02 22:38:08 -0300

    feat: Implement reactive form for categoria with validation and error handling

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './categoria.component.scss'
 })
 export class CategoriaComponent {
+  camposForm: FormGroup;
+
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    });
+  }
+  salvar(){
+    this.camposForm.markAllAsTouched();
+    if (this.camposForm.invalid) {
+    console.log(this.camposForm.value);
+    }
+  }
+
+  isCampoInvalido(campo: string): boolean {
+    const campoControl = this.camposForm.get(campo);
+    return (campoControl?.invalid && campoControl?.touched && campoControl.errors?.['required']) || false;
+  }
 
 }
